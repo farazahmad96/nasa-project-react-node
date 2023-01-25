6d0d1338d4e4b1e89d7e4b7790f0cc3b89245c83
Faraz <farazgojali@gmail.com>
2023-01-25 18:59:20 +0500

    client and server start at once

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
 
 const planetRouter = require('./routes/planets/planets.router');
 
@@ -8,7 +10,16 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public ')));
+
+
 app.use(planetRouter);
+
+app.get('/', (req, res) => {
+    res.sendFile(express.static(path.join(__dirname, '..', 'public', 'index.html')))
+});
 
 module.exports = app;

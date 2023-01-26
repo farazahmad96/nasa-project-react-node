ed60dcfdeedca9742b78cce53f8dafe658472631
Faraz <farazgojali@gmail.com>
2023-01-26 15:46:06 +0500

    submit launch added

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -10,8 +10,22 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
+
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+        return res.status(400).json({
+            error: "Missing required launch property",
+        });
+    }
+
     launch.launchDate = new Date(launch.launchDate);
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid launch date',
+        });
+    }
+
     addNewLaunch(launch);
+
     return res.status(201).json(launch);
 }
 

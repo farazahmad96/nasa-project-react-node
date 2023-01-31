2e69d38585770a038f40d9455054b86ea0e7e025
Faraz <farazgojali@gmail.com>
2023-01-31 17:23:00 +0500

    mongoose connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,22 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+mongoose.set('strictQuery', false);
+const MONGO_URL = 'mongodb+srv://nasa-api:C66SCVe8m8eX7iMm@cluster0.myfome9.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+    });
     loadPlanetsData();
 
     server.listen(PORT, () => {

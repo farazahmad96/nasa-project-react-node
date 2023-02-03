a3cac13e442c6a78c7468cdc2f75b83e3fc227a2
Faraz <farazgojali@gmail.com>
2023-02-03 17:30:52 +0500

    minimizing api load

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -20,7 +20,8 @@ saveLaunch(launch);
 
 const SPAECX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
-async function loadLaunchData() {
+async function populateLaunches() {
+
     console.log('Downloading launch data...');
     const response = await axios.post(SPAECX_API_URL, {
         query: {},
@@ -61,11 +62,31 @@ async function loadLaunchData() {
         };
 
         console.log(`${launch.flightNumber} ${launch.mission}`);
+
+        //TODO: populate launches collection
     }
 }
 
+async function loadLaunchData() {
+    const firstLaunch = await findLaunch({
+        flightNumber: 1,
+        rocket: "Falcon 1",
+        mission: "FalconSat"
+    });
+    if (firstLaunch) {
+        console.log('Launch data already loaded!');
+    } else {
+        await populateLaunches();
+    }
+
+}
+
+async function findLaunch(filter) {
+    return await launchesDatabase.findOne(filter);
+}
+
 async function existsLaunchWithId(launchId) {
-    return await launchesDatabase.findOne({
+    return await findLaunch({
         flightNumber: launchId,
     });
 }
